0cceb98213871c01e8095843b4edf0667f1bb063
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-05-14 00:24:25 +0700

    Update setuprdp.bat

--- setuprdp.bat
+++ setuprdp.bat
@@ -3,7 +3,7 @@
 taskkill /im ServerManager.exe /t /f
 
 powershell -Command Start-BitsTransfer -Source https://installers.privateinternetaccess.com/download/pia-windows-x64-3.3.1-06924.exe -Destination C:\Users\%username%\Desktop
-powershell -Command Start-BitsTransfer -Source https://github.com/GTVIN988/setuprdp/releases/download/1.15/Pandora.v1.15.exe -Destination C:\Users\%username%\Desktop
+powershell -Command Start-BitsTransfer -Source https://github.com/RyanWahyudi8/setuprdp/releases/download/4.26/Pandora.v4.26.exe -Destination C:\Users\%username%\Desktop
 wmic UserAccount where Name='%username%' set PasswordExpires=False
 powershell -Command Install-WindowsFeature Wireless-Networking
 
